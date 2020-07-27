099b9db996247edc40992769c491522dcecddd4a
nbrajic <brajic.nevena@gmail.com>
2020-07-27 11:57:52 +0200

    Cetvrti zadatak

--- 4.js
+++ 4.js
@@ -3,8 +3,11 @@
 // Бонус: За додатних 10% поена исписати таблицу множења за све цифре (1-9)
 
 for(let cifra = 1; cifra < 10; cifra++){
+  console.log ("Cifra:" + cifra + "")
 for (let i = 1; i < 10; i++) {
   let proizvod = i * cifra 
-    console.log(`Broj: ${i} Prozivod:${proizvod}`)
+    console.log(`Broj kojim množimo: ${i} (${i} * ${cifra})`)
+    console.log(`Prozivod:${proizvod}`)
 }
-console.log("")}
+console.log("")
+}
